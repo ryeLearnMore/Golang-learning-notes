3ef0f7a2306fe9f52895530a7944a9973ed6bc1e
rye <rye_learnmore@163.com>
2019-11-26 16:17:03 +0800

    add part of func

---
+++ src/code.rye.com/studyGolang/lesson2/func/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+// 定义一个不需要参数也没有返回值的函数，sayHello
+func sayHello()  {
+	fmt.Println("Hello!")
+}
+
+func sayHello2(name string)  {
+	fmt.Println("Hello ", name)
+}
+
+func intSum(a int, b int) int {
+	ret := a + b
+	return ret
+}
+
+func intSum2(a int, b int) (ret int) {
+	ret = a + b
+	return ret // 或者可以不写ret，直接return
+}
+
+// 函数接收可变参数,在参数后面加...表示可变参数
+// 可变参数在函数中是切片类型
+func intSum3(a ...int) int {
+	// fmt.Println(a) // []
+	// fmt.Printf("%T\n", a) // []int
+	ret := 0
+	for _, arg := range a{
+		ret += arg
+	}
+	return ret
+}
+
+// 注：固定参数和可变参数同时出现时，可变参数要放在最后
+func intSum4(a int, b ...int) int {
+	// fmt.Println(a) // []
+	// fmt.Printf("%T\n", a) // []int
+	ret := a
+	for _, arg := range b{
+		ret += arg
+	}
+	return ret
+}
+
+func main()  {
+	// 函数调用
+	sayHello()
+
+	name := "rye"
+	sayHello2(name)
+
+	r := intSum(10, 20)
+	fmt.Println(r)
+
+	fmt.Println(intSum3(10, 20, 3, 4))
+	fmt.Println(intSum4(1, 20, 3, 4)) 
+}
