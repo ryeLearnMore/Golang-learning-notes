866b1d53af2b2c12bf32e9bdb95ce64c52813db6
rye <rye_learnmore@163.com>
2020-01-11 20:33:21 +0800

    add temp

---
+++ src/code.rye.com/studyGolang/lesson4/02package/use_pkg/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "GoProjects/Golang-learning-notes/src/code.rye.com/studyGolang/lesson4/02package/math_pkg"
+// import "D:/GoProjects/Golang-learning-notes/src/code.rye.com/studyGolang/lesson4/02package/math_pkg"
+
+func main()  {
+	math_pkg.Add(100, 200)
+}
