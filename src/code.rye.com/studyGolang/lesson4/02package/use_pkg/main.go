b80ef62a7c6311a81323c565e2a34954bd3ccf79
rye <rye_learnmore@163.com>
2020-01-11 20:34:34 +0800

    add temp

--- src/code.rye.com/studyGolang/lesson4/02package/use_pkg/main.go
+++ src/code.rye.com/studyGolang/lesson4/02package/use_pkg/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "GoProjects/Golang-learning-notes/src/code.rye.com/studyGolang/lesson4/02package/math_pkg"
-// import "D:/GoProjects/Golang-learning-notes/src/code.rye.com/studyGolang/lesson4/02package/math_pkg"
+// 这个文件是干什么的
+import "fmt"
 
-func main()  {
+// 导入包时起别名
+// import m "code.oldboy.com/studygolang/day04/02package/math_pkg"
+import "code.rye.com/studyGolang/lesson4/02package/math_pkg"  // 一定要设置GOPATH路径
+
+// import (
+// 	"fmt"
+
+// 	"code.rye.com/studyGolang/lesson4/02package/math_pkg"
+// )
+
+const Mode = 1
+
+func main() {
 	math_pkg.Add(100, 200)
+	stu := math_pkg.Student{Name: "haojie", Age: 18}
+	// m.Add(100, 200)
+	// stu := m.Student{Name: "haojie", Age: 18}
+	fmt.Println(stu.Name)
+	fmt.Println(stu.Age)
 }
