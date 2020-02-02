a80a844083e827b465a3a6bb8c2c4cc2f25298b6
rye <rye_learnmore@163.com>
2020-02-02 17:27:36 +0800

    add 用go语言对mysql进行增删改查，同时包括预处理，在windows上使用MySQL

---
+++ src/code.rye.com/studyGolang/lesson9/02mysql_demo2/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql" // init()
+)
+
+// Go 连接MySQL示例
+
+var db *sql.DB // 是一个连接池对象
+
+func initDB() (err error) {
+	// 数据库信息
+	dsn := "root:1234@tcp(127.0.0.1:3306)/sql_test"
+	// 连接数据库
+	// 注意！！！这里不要使用:=，我们是给全局变量赋值，然后在main函数中使用全局变量db
+	db, err = sql.Open("mysql", dsn) // 不会校验用户名和密码是否正确
+	if err != nil {                  // dsn格式不正确的时候会报错
+		return
+	}
+	err = db.Ping() // 检测用户名和密码是否正确，尝试连接数据库
+	if err != nil {
+		return
+	}
+
+	// 设置数据库连接池的最大连接数
+	db.SetMaxOpenConns(10)
+	// 设置最大闲置连接数
+	db.SetMaxIdleConns(5)
+	return
+}
+
+type user struct {
+	id   int
+	name string
+	age  int
+}
+
+// 查询单个记录
+func queryOne(id int) {
+	var u1 user
+	// 1. 写查询单条记录的sql语句
+	// sqlStr := `select id, name, age from user where id=1;`
+	sqlStr := `select id, name, age from user where id=?;`
+	// 2. 执行
+	rowObj := db.QueryRow(sqlStr, id) // 从连接池里拿一个连接出来去数据库中查
+	// 3. 拿到结果
+	rowObj.Scan(&u1.id, &u1.name, &u1.age) // 必须对rowObj对象调用Scan方法，使其放入连接池中。
+	// 4. 打印结果
+	fmt.Printf("u1: %#v\n", u1)
+}
+
+// 查询多条
+func queryMore(n int) {
+	// 1. SQL 语句
+	sqlStr := "select id, name, age from user where id > ?;"
+	// 2. 执行
+	rows, err := db.Query(sqlStr, n)
+	if err != nil {
+		fmt.Printf("exec %s query failed: %v\n", sqlStr, err)
+		return
+	}
+	// 3. 一定要关闭
+	defer rows.Close()
+	// 4. 循环取值
+	for rows.Next() {
+		var u1 user
+		err := rows.Scan(&u1.id, &u1.name, &u1.age)
+		if err != nil {
+			fmt.Printf("scan failed, err: %v\n", err)
+			return
+		}
+		fmt.Printf("u1: %#v\n", u1)
+	}
+}
+
+// 插入
+func insert() {
+	// 1. 写SQL语句
+	sqlStr := "insert into user(name, age) values(\"刘亦菲\", 18);"
+	// 2. exec
+	ret, err := db.Exec(sqlStr)
+	if err != nil {
+		fmt.Printf("insert failed, err: %v\n", err)
+		return
+	}
+	// 如果是插入数据的操作，能够拿到插入数据的id
+	id, err := ret.LastInsertId()
+	if err != nil {
+		fmt.Printf("get id failed: %v\n", err)
+		return
+	}
+	fmt.Println("id: ", id)
+}
+
+// 更新操作
+func updateRow(newAge int, id int) {
+	sqlStr := "update user set age=? where id=?;"
+	ret, err := db.Exec(sqlStr, newAge, id)
+	if err != nil {
+		fmt.Printf("update failed: %v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected()
+	if err != nil {
+		fmt.Printf("get id failed: %v\n", err)
+		return
+	}
+	fmt.Printf("更新了%d行数据\n", n)
+}
+
+// 删除
+func deleteRow(id int) {
+	sqlStr := "delete from user where id=?;"
+	ret, err := db.Exec(sqlStr, id)
+	if err != nil {
+		fmt.Printf("delete failed: %v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected()
+	if err != nil {
+		fmt.Printf("get id failed: %v\n", err)
+		return
+	}
+	fmt.Printf("删除了%d行数据\n", n)
+}
+
+// 预处理方式插入多条数据
+func prepareInsert() {
+	sqlStr := "insert into user(name, age) values(?, ?);"
+	stmt, err := db.Prepare(sqlStr) // 先把SQL语句发给MySQL与处理一下
+	if err != nil {
+		fmt.Printf("prepare failed: %v\n", err)
+		return
+	}
+	defer stmt.Close()
+	// 后续只要拿到stmt去执行一些操作即可
+	var m = map[string]int{
+		"万茜": 30,
+		"宋茜": 31,
+		"成果": 20,
+	}
+	for k, v := range m {
+		stmt.Exec(k, v) // 后续只需要传值
+	}
+}
+
+func main() {
+	err := initDB()
+	if err != nil {
+		fmt.Printf("init DB failed: %v\n", err)
+	}
+	fmt.Println("连接数据库成功！")
+
+	// queryOne(2)
+	// queryMore(0)
+	// insert()
+	// updateRow(18, 1)
+	// deleteRow(2)
+	prepareInsert()
+}
